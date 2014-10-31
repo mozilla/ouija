291699a63fde293f4037c92271a9c2ff32fa9961
Dan Minor <dminor@mozilla.com>
2014-10-31 09:55:36 -0400

    Make log and bugid fields text

--- sql/schema.sql
+++ sql/schema.sql
@@ -5,14 +5,14 @@ USE ouija;
 
 CREATE TABLE IF NOT EXISTS `testjobs` (
   `id` int(11) NOT NULL,
-  `log` varchar(1024) NOT NULL,
+  `log` text NOT NULL,
   `slave` varchar(64) NOT NULL,
   `result` varchar(32) NOT NULL,
   `duration` int(11) NOT NULL,
   `platform` varchar(32) NOT NULL,
   `buildtype` varchar(32) NOT NULL,
   `testtype` varchar(64) NOT NULL,
-  `bugid` varchar(32) NOT NULL,
+  `bugid` text NOT NULL,
   `branch` varchar(64) NOT NULL,
   `revision` varchar(32) NOT NULL,
   `date` datetime NOT NULL,
