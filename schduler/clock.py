7f8f71f68d348ba71e598d8f4bebb5d4e14ec1a8
Joel Maher <joel.maher@gmail.com>
2016-09-15 13:38:53 -0400

    remove hours, add minutes

--- schduler/clock.py
+++ schduler/clock.py
@@ -26,12 +26,12 @@ def trigger_failures():
 def timed_trigger_updatedb():
     q.enqueue(trigger_migratedb)
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=17, minutes=40)
+@sched.scheduled_job('cron', day_of_week='mon-sun', minutes=5)
 def timed_trigger_updatedb_sec():
     q.enqueue(trigger_migratedb)
 
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=17, minutes=45)
+@sched.scheduled_job('cron', day_of_week='mon-sun', minutes=10)
 def timed_trigger_failures():
     q.enqueue(trigger_failures)
 
